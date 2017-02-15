f791466b588a9be426295b54d0ff7504f75dd39b
Lan Kyoung Hong <LanKyoungHong@lan-ui-air.home>
2017-02-15 01:28:56 -0500

    added code for didSelectRowAtIndexPath, but exception occurs when selecting row

--- Homework1/ViewController.swift
+++ Homework1/ViewController.swift
@@ -8,11 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var picTitle: UILabel!
     
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var picker: UIPickerView!
     @IBOutlet var thePicList: PictureList!
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell : UITableViewCell? = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if (cell == nil)
+        {
+            cell = UITableViewCell.init(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        }
+        let thisPicture : PictureStruct = thePicList.pictureList[indexPath.row]
+        cell!.textLabel?.text = thisPicture.title
+        return cell!
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return thePicList.count()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let thisPicture : PictureStruct = thePicList.pictureList[indexPath.row]
+        let alert : UIAlertController = UIAlertController.init(title: thisPicture.fileName, message: thisPicture.title, preferredStyle: UIAlertControllerStyle.actionSheet)
+        alert.show(self, sender: self)
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return 1
@@ -29,6 +53,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
         picTitle.text = thePicList.pictureList[row].title
     }
     
@@ -38,6 +63,7 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
